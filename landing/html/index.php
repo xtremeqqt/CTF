313b208121655174501775d42dfbeff768ef32a3
root <root@catsec.localdomain>
2017-03-02 22:09:29 +0000

    Added landing page

---
+++ landing/html/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+
+<body>
+	<h1>Welcome to C.A.T CTF</h1>
+	
+	<p>Within the applications present on this server there are a number of flags to be found. Flags follow the format flag{xyz}. Once found, these flags can be submitted at the following page <a href="">placeholder link</a></p>
+	<br>
+	<h2>Mission breifing</h2>
+	<p>Hey there. We have recently been put under extreme duress due to actions taken by a supposedly anonymous user on the internet. The only information we have on him is the email address he uses to register on sites. It is malicious@catch.me.</p>
+	<p>If possible, we would like you to unmask this unknown assailant through any means nessecary. Good luck, we're counting on you.</p>
+
+</body>
+</html>
