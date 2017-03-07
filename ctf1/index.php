8935790ed07d8d8e01747e3ee41edd828a2366ad
root <root@localhost.localdomain>
2017-03-07 14:14:28 -0500

    Anutha ting fix

--- ctf1/index.php
+++ ctf1/index.php
@@ -6,13 +6,13 @@
 <body>
 <h1>C.A.T Sec CTF-1</h1>
 <div class="row">
-    <div class="medium-3 column">
+    <div class="large-2 column">
         <p style="text-align: center">ting 1</p>
     </div>
-    <div class="large-6 column">
+    <div class="large-8 column">
         <p style="text-align: center">ting 2</p>
     </div>
-    <div class="medium-3  columns">
+    <div class="large-2  columns">
         <p style="text-align: center">ting 3</p>
     </div>
 </div>
