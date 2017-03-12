e04c6ea0722aaf08eedc8e17f1c58ce29b7744cc
root <root@localhost.localdomain>
2017-03-12 14:11:40 -0400

    Fixed and styling

--- ctf1/inbox.php
+++ ctf1/inbox.php
@@ -21,11 +21,13 @@
         <p style="text-align: center">ting 1</p>
     </div>
     <div class="large-8 column">
+        <h2>Inbox</h2>
+        <hr>
         <?php
             if (mysqli_num_rows($result) > 0) {
                 // output data of each row
                 while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<p>".$row['subject']."</p>";
+                    echo "<a>".$row['subject']."</a>";
                 }
             } else {
                 echo "Fail";
