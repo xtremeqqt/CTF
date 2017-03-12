87afdf9abb6762ba657bf6eb9760207d561c78e0
root <root@localhost.localdomain>
2017-03-12 14:00:32 -0400

    Test

--- ctf1/inbox.php
+++ ctf1/inbox.php
@@ -22,6 +22,7 @@
     </div>
     <div class="large-8 column">
         <?php
+            echo mysqli_num_rows($result);
             if (mysqli_num_rows($result) > 0) {
                 // output data of each row
                 while ($row = mysqli_fetch_assoc($result)) {
