11c4ad9fba489a2f5fb6b2fa52a3f637363247a7
root <root@localhost.localdomain>
2017-03-12 14:05:10 -0400

    Another test

--- ctf1/inbox.php
+++ ctf1/inbox.php
@@ -6,7 +6,7 @@
     $conn->select_db('Gratification');
 
     $sql = "SELECT * FROM 'emails'";
-    $result = mysqli_query($conn, $sql);
+    $result = $conn->query($sql);
 
 ?>
 
@@ -22,7 +22,6 @@
     </div>
     <div class="large-8 column">
         <?php
-            echo mysqli_num_rows($result);
             if (mysqli_num_rows($result) > 0) {
                 // output data of each row
                 while ($row = mysqli_fetch_assoc($result)) {
