3036509563aea5f36fb22bf903309cfcbd2085e0
root <root@localhost.localdomain>
2017-03-12 15:07:30 -0400

    Added email view

--- ctf1/inbox.php
+++ ctf1/inbox.php
@@ -1,18 +1,17 @@
 <html>
 
 <?php
-    include_once('includes/config.php');
+include_once('includes/config.php');
 
-    $conn->select_db('Gratification');
+$conn->select_db('Gratification');
 
-    $sql = "SELECT subject FROM emails";
-    $result = $conn->query($sql);
+
 
 ?>
 
 <head>
-<link rel="stylesheet" href="css/foundation.min.css">
-<title>C.A.T Sec</title>
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <title>C.A.T Sec</title>
 </head>
 <body>
 <h1>C.A.T Sec CTF-1</h1>
@@ -21,17 +20,33 @@
         <p style="text-align: center">ting 1</p>
     </div>
     <div class="large-8 column">
-        <h3>Inbox</h3>
-        <hr>
         <?php
+        if (isset($_GET['email'])) {
+            $email = mysqli_real_escape_string($_GET['email']);
+            $sql = "SELECT author, subject, body FROM emails where id = $email";
+            $result = $conn->query($sql);
+            if (mysqli_num_rows($result) == 0) {
+                $row = mysqli_fetch_row($result);
+                echo "<h3>" . $row['subject'] . "</h3>";
+                echo "<hr>";
+                echo "<p>" . $row['body'] . "</p>";
+            }
+
+        } else {
+            echo "<h3 > Inbox</h3 >";
+            echo "<hr>";
+            $sql = "SELECT id, subject FROM emails";
+            $result = $conn->query($sql);
             if (mysqli_num_rows($result) > 0) {
                 // output data of each row
                 while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<a>".$row['subject']."</a><br />";
+                    $id = $row['id'];
+                    echo "<a href='inbox.php?email=' . $id>" . $row['subject'] . "</a><br />";
                 }
             } else {
                 echo "Fail";
             }
+        }
         ?>
 
     </div>
